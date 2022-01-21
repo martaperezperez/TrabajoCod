ebb6e9e667685239436c928c8eaf9eee2040e15c
pinha <pinhaeguia@gmail.com>
2022-01-21 09:49:59 +0100

    faltaba una "B"

---
+++ src/com/COD/Picao.java
@@ -0,0 +1,15 @@
+package com.COD;
+
+public class Picao {
+
+    String lechuga;
+    String tomate;
+    String pan;
+    String salsa_gay;
+    String embutido;
+
+    public void sandwich(){
+        System.out.println("De que pingas quieres el sandwich");
+    }
+
+}
